0405e6f6e8d7c75c4f9ede5c050528ae098860ef
hardikshah197 <hardikshah.hs2015@gmail.com>
2020-08-10 12:41:34 +0530

    Updates

---
+++ precache-manifest.30acb01e875d852939f5462bb1c33c72.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "627ad2c1c0eea3accadb39d9b8052e23",
+    "url": "/Covid-Opp/index.html"
+  },
+  {
+    "revision": "30219f3e3280bcd9bcf3",
+    "url": "/Covid-Opp/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "1830fa293297875ecffc",
+    "url": "/Covid-Opp/static/css/main.21429baf.chunk.css"
+  },
+  {
+    "revision": "30219f3e3280bcd9bcf3",
+    "url": "/Covid-Opp/static/js/2.942e4608.chunk.js"
+  },
+  {
+    "revision": "bc645b8808bddaa3c0c8d72dff721b3b",
+    "url": "/Covid-Opp/static/js/2.942e4608.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d2284cdb9d0e8bc4e03",
+    "url": "/Covid-Opp/static/js/3.f5388196.chunk.js"
+  },
+  {
+    "revision": "1830fa293297875ecffc",
+    "url": "/Covid-Opp/static/js/main.ea20eff5.chunk.js"
+  },
+  {
+    "revision": "2351c055a522544330d0",
+    "url": "/Covid-Opp/static/js/runtime-main.5c63301a.js"
+  },
+  {
+    "revision": "041c3602b153db59bd65c610c9601451",
+    "url": "/Covid-Opp/static/media/23andme.041c3602.svg"
+  },
+  {
+    "revision": "1a8d8814fb3fc2f6626e23bb68de98eb",
+    "url": "/Covid-Opp/static/media/activision.1a8d8814.svg"
+  },
+  {
+    "revision": "f54e3b07795bbb638db77ad9c5084647",
+    "url": "/Covid-Opp/static/media/airbnb-logo.f54e3b07.svg"
+  },
+  {
+    "revision": "8e000dbafb4360d9f75e5e6a0950a74e",
+    "url": "/Covid-Opp/static/media/airbnb.8e000dba.svg"
+  },
+  {
+    "revision": "5b943ec180b3a6802fcc30ac4555c7c6",
+    "url": "/Covid-Opp/static/media/akamaitechnologies.5b943ec1.svg"
+  },
+  {
+    "revision": "a63ac2d11f63fd3e28f230448b163cf9",
+    "url": "/Covid-Opp/static/media/atlassian.a63ac2d1.svg"
+  },
+  {
+    "revision": "6c081dee19476fd606c8b817360dc104",
+    "url": "/Covid-Opp/static/media/blend.6c081dee.svg"
+  },
+  {
+    "revision": "83903d9abf025a797295e7d09d954921",
+    "url": "/Covid-Opp/static/media/cloudera.83903d9a.svg"
+  },
+  {
+    "revision": "8702940c0649177b41e607e5696c5387",
+    "url": "/Covid-Opp/static/media/doordash.8702940c.svg"
+  },
+  {
+    "revision": "5c58d0e1d2ec48e766846af99eaeb6f8",
+    "url": "/Covid-Opp/static/media/flags.5c58d0e1.png"
+  },
+  {
+    "revision": "27670c535824abb6ae7263d60fdc8a86",
+    "url": "/Covid-Opp/static/media/flags@2x.27670c53.png"
+  },
+  {
+    "revision": "b3854ed2f331f80578a12c2c25e7d53b",
+    "url": "/Covid-Opp/static/media/honey.b3854ed2.svg"
+  },
+  {
+    "revision": "225097e1399970095ad361116fe2f8c1",
+    "url": "/Covid-Opp/static/media/ibm.225097e1.jpg"
+  },
+  {
+    "revision": "2bb1a2af008d5c828b498a058316983a",
+    "url": "/Covid-Opp/static/media/jpmorgan.2bb1a2af.svg"
+  },
+  {
+    "revision": "06fc77cf54ccf4868ead7917d6edf890",
+    "url": "/Covid-Opp/static/media/lime.06fc77cf.svg"
+  },
+  {
+    "revision": "24bdca955d3d8a90c63581dd2150b705",
+    "url": "/Covid-Opp/static/media/lyft.24bdca95.svg"
+  },
+  {
+    "revision": "8cc141f884b5d60e948f251ad1e4db39",
+    "url": "/Covid-Opp/static/media/mongodb.8cc141f8.svg"
+  },
+  {
+    "revision": "28d3ddfcfeba46ca5fc019b6042c9330",
+    "url": "/Covid-Opp/static/media/nerdwallet.28d3ddfc.svg"
+  },
+  {
+    "revision": "12431a6a2430efac1fb5baf2b704cf57",
+    "url": "/Covid-Opp/static/media/pinterest.12431a6a.svg"
+  },
+  {
+    "revision": "df6e607a10bcb1ae2a59618f7c0c35be",
+    "url": "/Covid-Opp/static/media/plaid.df6e607a.svg"
+  },
+  {
+    "revision": "150eed6cd7c96ac3e95cdcc84483902e",
+    "url": "/Covid-Opp/static/media/samsara.150eed6c.svg"
+  },
+  {
+    "revision": "23d905fe5a95321b02b05a36ed9db035",
+    "url": "/Covid-Opp/static/media/stripe.23d905fe.svg"
+  },
+  {
+    "revision": "fa74ce7a8e50f21be6089d6f74b09556",
+    "url": "/Covid-Opp/static/media/toast.fa74ce7a.svg"
+  },
+  {
+    "revision": "a1238cf9a8b12238f1c135b3af41848d",
+    "url": "/Covid-Opp/static/media/twitch.a1238cf9.svg"
+  }
+]);
